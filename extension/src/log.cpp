12e56918c60cf0aa5227931f8b67469aa4cd24c8
László Major <laszlo.major@outlook.com>
2016-10-08 00:28:06 +0200

    Using OS independent separator for file path.

--- extension/src/log.cpp
+++ extension/src/log.cpp
@@ -1,14 +1,20 @@
 #include "log.h"
 
+#include "Poco/Path.h"
 #include "Poco/DateTimeFormatter.h"
 #include "Poco/LocalDateTime.h"
 
 namespace r3 {
 namespace log {
+
+namespace {
+    const std::string LOGGER_NAME = "r3_extension_log";
+}
+
     std::shared_ptr<spdlog::logger> logger;
 
     bool initialze(const std::string& extensionFolder, const std::string& logLevel) {
-        logger = spdlog::rotating_logger_mt("r3_extension", fmt::format("{}\\{}", extensionFolder, getLogFileName()), 1024 * 1024 * 20, 1);
+        logger = spdlog::rotating_logger_mt(LOGGER_NAME, fmt::format("{}{}{}", extensionFolder, Poco::Path::separator(), getLogFileName()), 1024 * 1024 * 20, 1);
         logger->flush_on(spdlog::level::trace);
         logger->set_level(getLogLevel(logLevel));
         return true;
@@ -18,8 +24,8 @@ namespace log {
     };
 
     std::string getLogFileName() {
-        std::string fileName = "r3_extension_log_";
-        Poco::DateTimeFormatter::append(fileName, Poco::LocalDateTime(), "%Y-%m-%d_%H-%M-%S");
+        std::string fileName = LOGGER_NAME;
+        Poco::DateTimeFormatter::append(fileName, Poco::LocalDateTime(), "_%Y-%m-%d_%H-%M-%S");
         return fileName;
     }
 
