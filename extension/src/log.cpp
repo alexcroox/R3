989d651fb10b24de3aa57e8240dc69827a26c83f
László Major <laszlo.major@outlook.com>
2016-10-08 00:45:51 +0200

    Made unused logging functions private.

--- extension/src/log.cpp
+++ extension/src/log.cpp
@@ -13,6 +13,18 @@ namespace {
 
     std::shared_ptr<spdlog::logger> logger;
 
+    spdlog::level::level_enum getLogLevel(const std::string& logLevel) {
+        if (logLevel == "debug") { return spdlog::level::debug; }
+        if (logLevel == "trace") { return spdlog::level::trace; }
+        return spdlog::level::info;
+    }
+
+    std::string getLogFileName() {
+        std::string fileName = LOGGER_NAME;
+        Poco::DateTimeFormatter::append(fileName, Poco::LocalDateTime(), "_%Y-%m-%d_%H-%M-%S");
+        return fileName;
+    }
+
     bool initialze(const std::string& extensionFolder, const std::string& logLevel) {
         logger = spdlog::rotating_logger_mt(LOGGER_NAME, fmt::format("{}{}{}", extensionFolder, Poco::Path::separator(), getLogFileName()), 1024 * 1024 * 20, 1);
         logger->flush_on(spdlog::level::trace);
@@ -22,18 +34,6 @@ namespace {
 
     void finalize() {
     };
-
-    std::string getLogFileName() {
-        std::string fileName = LOGGER_NAME;
-        Poco::DateTimeFormatter::append(fileName, Poco::LocalDateTime(), "_%Y-%m-%d_%H-%M-%S");
-        return fileName;
-    }
-
-    spdlog::level::level_enum getLogLevel(const std::string& logLevel) {
-        if (logLevel == "debug") { return spdlog::level::debug; }
-        if (logLevel == "trace") { return spdlog::level::trace; }
-        return spdlog::level::info;
-    }
 
 } // namespace log
 } // namespace r3
